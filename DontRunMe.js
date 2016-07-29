0ff73deb19d2335118d518ac11b540b6b12dcdc3
Ilteris EROGLU <linuxgemini@users.noreply.github.com>
2016-07-29 17:44:39 +0300

    Update DontRunMe.js

--- DontRunMe.js
+++ DontRunMe.js
@@ -3,9 +3,9 @@
 let crypto = require('crypto');
 
 function randomValueHex (len) {
-  return crypto.randomBytes(Math.ceil(len/2))
-    .toString('hex') // convert to hexadecimal format
-    .slice(0,len);   // return required number of characters
+    return crypto.randomBytes(Math.ceil(len/2))
+        .toString('hex') // convert to hexadecimal format
+        .slice(0,len);   // return required number of characters
 };
 
 let a = randomValueHex(32);
